c07d3f46a1a861cf1022c5e171c1c75d51142d98
TomerHP <tomerhp@gmail.com>
2016-06-21 12:46:48 +0300

    21062016.1246

--- run_analysis.R
+++ run_analysis.R
@@ -8,26 +8,33 @@
   
   ##Merging into one big table with 10299 observations and 561 variables
   All<-rbind(train,test)
-
-   
+  
+  ##Add ID column
+  test_subjects<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt",col.names=c("ID"))
+  train_subjects<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt",col.names=c("ID"))
+  All_subjects<-rbind(train_subjects, test_subjects) 
+  All<-cbind(All_subjects,All)
+  
 ##Extracts only the measurements on the mean and staandard deviation for each measurement.
   All_sum<-rbind(apply(All,2,mean),apply(All,2,sd))
 
   
 ##Uses descriptive activity names to name the activities in the data set
-  Activities<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt",colClasses = c("NULL","character"))
-  test_subjects<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
-  train_subjects<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
-  All_subjects<-rbind(train_subjects, test_subjects)
-  names(All_subjects)<-c("ID")
-  Y_test<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt")
+  Activities<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt",col.names = c("Activity_code","Acitivity"))
+  Y_test<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt",col.names="Activity_code")
+  Y_train<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt",col.names="Activity_code")
+  Y_all<-rbind(Y_train,Y_test)
+  Y_merged<-merge(Y_all,Activities)
+  All<-cbind(All[1],Y_merged[2],All[2:562])
   
 ##Appropriately labels the data set with descriptive variable names.
   Lables<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt",colClasses = c("NULL","character"))
   Lables_V<-sapply(Lables,as.character)
   names(All)<-Lables_V
-  
-  
-  
-##From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+    
+##From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+  
+  
+  
+  
