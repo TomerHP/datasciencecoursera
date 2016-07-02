eeeade03f9f952514a96c82ab85bcbb9744f3aa0
tomerHP <TomerHP@gmail.com>
2016-07-02 19:32:51 +0300

    1932

--- Readme.md
+++ Readme.md
@@ -1,5 +1,18 @@
-##Notes about Run_Analysis.R script
+##General notes about Run_Analysis.R script
 
-
+This scrypt does the following manipulations on the data:
+	1. Merges the training and the test sets to create one data set.
+    2. Extracts only the measurements on the mean and standard deviation for each measurement.
+    3. Uses descriptive activity names to name the activities in the data set
+    4. Appropriately labels the data set with descriptive variable names.
+    5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+	
+Make sure you have extracted the content of the zipped file "getdata_projectfiles_UCI HAR Dataset.zip" into your working directory 
+so that you have a folder named UCI HAR Dataset with all subfolders and files. 
 
 ##Code book
+	
+	#variables names and descriptions
+		
+		Name		|	Description
+		
--- run_analysis.R
+++ run_analysis.R
@@ -1,30 +1,30 @@
 ##1. Merges the training and the test sets to create one data set.
 
   ## Import train/X_train.txt (training) AND  test/X_test.txt (test)
-  train<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
+  train<-read.table(header = FALSE,file = "UCI HAR Dataset/train/X_train.txt")
   ##7351 observations of  561 variables
-  test<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
+  test<-read.table(header = FALSE,file = "UCI HAR Dataset/test/X_test.txt")
   ##2946 observations of  561 variables
   
   ##Merging into one big table with 10299 observations and 561 variables
   All<-rbind(train,test)
   
 ##2. Extracts only the measurements on the mean and standard deviation for each measurement.
-  Lables<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt",colClasses = c("NULL","character"))
+  Lables<-read.table(header = FALSE,file = "UCI HAR Dataset/features.txt",colClasses = c("NULL","character"))
   Lables_V<-sapply(Lables,as.character)
   names(All)<-Lables_V
   extracted<-All[Lables_V[grep("mean|std",Lables_V)]]
 
 ##3. Uses descriptive activity names to name the activities in the data set
-  Activities<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt",col.names = c("Activity_code","Activity"))
-  Y_test<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt",col.names="Activity_code")
-  Y_train<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt",col.names="Activity_code")
+  Activities<-read.table(header = FALSE,file = "UCI HAR Dataset/activity_labels.txt",col.names = c("Activity_code","Activity"))
+  Y_test<-read.table(header = FALSE,file = "UCI HAR Dataset/test/y_test.txt",col.names="Activity_code")
+  Y_train<-read.table(header = FALSE,file = "UCI HAR Dataset/train/y_train.txt",col.names="Activity_code")
   Y_all<-rbind(Y_train,Y_test)
   Y_merged<-merge(Y_all,Activities)
   
 ##4. Appropriately labels the data set with descriptive variable names.
-  test_subjects<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt",col.names=c("Subject"))
-  train_subjects<-read.table(header = FALSE,file = "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt",col.names=c("Subject"))
+  test_subjects<-read.table(header = FALSE,file = "UCI HAR Dataset/test/subject_test.txt",col.names=c("Subject"))
+  train_subjects<-read.table(header = FALSE,file = "UCI HAR Dataset/train/subject_train.txt",col.names=c("Subject"))
   All_subjects<-rbind(train_subjects, test_subjects) 
   All<-cbind(All_subjects,All)
   extracted<-cbind(All_subjects,Y_merged[2],extracted)
